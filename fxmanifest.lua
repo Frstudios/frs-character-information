de0f88e94045641f25553e41bf4ab937b25fcdcb
Frstudios <frstudios24@gmail.com>
2024-12-26 03:21:13 +0100

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'frs-dev'
+description 'Frs-dev'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+    'html/assets/audio.mp3'
+}
